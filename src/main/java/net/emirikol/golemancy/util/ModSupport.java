8b5909326ef08683a1564170b82a6c36d974f7a2
unilock <unilock@fennet.rentals>
2024-06-05 13:30:25 -0400

    Fix Croparia support (is this even needed?)
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/net/emirikol/golemancy/util/ModSupport.java
+++ src/main/java/net/emirikol/golemancy/util/ModSupport.java
@@ -4,7 +4,7 @@ import net.minecraft.item.ItemStack;
 
 public class ModSupport {
 
-    public static final String CROPARIA_IDENTIFIER = "com.croparia.mod.item.CropsSeed";
+    public static final String CROPARIA_IDENTIFIER = "com.defacto34.croparia.api.crop.CropSeed";
 
     public static boolean isModdedSeed(ItemStack stack) {
         boolean croparia = stack.getItem().getClass().getName().equals(CROPARIA_IDENTIFIER);
